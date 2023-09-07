ac441064337ebfb1755d8567cb485b272f6c5c43
Anais-A <anais.artiges@free.fr>
2023-09-07 12:00:09 -0400

    Modified pydantic code for a more flexible management of data

--- SDLGenerator.py
+++ SDLGenerator.py
@@ -1,19 +1,36 @@
+################################################################################
+### mtrk project - SDL file generator allowing to simply define MRI pulse    ###
+### sequences that can be read by the mtrk project simulator and driver      ###
+### sequence.                                                                ###
+### Version 0.0.1                                                            ###
+### Anais Artiges and the mtrk project team at NYU - 09/07/2023              ###
+################################################################################  
+
 from __future__ import annotations
 
 from typing import List, Optional, Union
-from typing_extensions import Literal, TypedDict
-from pydantic import BaseModel
+from typing_extensions import Literal, Any
+from pydantic import BaseModel, SerializeAsAny
 import json
+import jsbeautifier
+import re
+from pprint import pprint
+from devtools import debug
 
+### file section
 class File(BaseModel):
     format: str
     version: int
     measurement: str
     system: str
 
+
+### settings section
 class Settings(BaseModel):
     readout_os: int
 
+
+### infos section
 class Infos(BaseModel):
     description: str
     slices: int
@@ -22,95 +39,168 @@ class Infos(BaseModel):
     seqstring: str
     reconstruction: str
 
-class Main(BaseModel):
-    print_message: str
-    steps: List[Step]
+
+### instructions section
+step_subclass_registry = {}
+
 
 class Step(BaseModel):
     action: str
+    
+    def __init_subclass__(cls, **kwargs: Any) -> None:
+        super().__init_subclass__(**kwargs)
+        step_subclass_registry[cls.__name__] = cls    
 
-class Loop(Step):
+    class Config:
+        extra = "allow" 
+
+
+class HasSteps():
+    def __init__(self, **kwargs):
+        for index in range(len(kwargs['steps'])):
+            current_step = kwargs['steps'][index]
+            if isinstance(current_step, dict):
+                item_step_keys = sorted(current_step.keys())
+                for name, cls in step_subclass_registry.items():
+                    registery_step_keys = sorted(cls.model_fields.keys())
+                    if item_step_keys == registery_step_keys:
+                        try:
+                            current_step = cls(**current_step)
+                        except: 
+                            pass
+                        break
+                else:
+                    raise Exception(f"Unknown step action \"{current_step['action']}\"")
+                kwargs['steps'][index] = current_step
+        super().__init__(**kwargs)
+
+class Instruction(HasSteps,BaseModel):
+    print_counter: Optional[str] = "off"
+    print_message: str
+
+    steps: List[SerializeAsAny[Step]]
+
+
+class Loop(HasSteps,Step):
     action: Literal["loop"]
     counter: int
     range: int
     steps: List[Step]
 
+
 class RunBlock(Step):
     action: Literal["run_block"]
     block: str
 
-class Calc(BaseModel):
-    action: Literal["calc", "init", "sync", "grad", "rf", "adc", "mark", "submit"]
-    type: Optional[str] = None
-    float: Optional[int] = None
-    increment: Optional[int] = None
-    gradients: Optional[str] = None
-    object: Optional[str] = None
-    time: Optional[int] = None
-    axis: Optional[str] = None
-    added_phase: Optional[AddedPhase] = None
-    amplitude: Optional[Union[str, AmplitudeItem]] = None
-    frequency: Optional[int] = None
-    phase: Optional[int] = None
-    mdh: Optional[Mdh] = None
 
-class BlockPhaseEncoding(BaseModel):
-    print_counter: str
-    print_message: str
-    steps: List[Step]
+class Calc(Step):
+    action: Literal["calc"]
+    type: str
+    float: int
+    increment: int
+
+
+class Init(Step):
+    action: Literal["init"]
+    gradients: str
+
+
+class Sync(Step):
+    action: Literal["sync"]
+    object: str
+    time: int
+
+
+class Grad(Step):
+    action: Literal["grad"]
+    axis: str
+    object: str
+    time: int
+
+
+class GradWithAmplitude(Grad):
+    amplitude: Union[str, Amplitude]
+
+
+class Amplitude(BaseModel):
+    type: str
+    equation: str
+
+
+class Rf(Step):
+    action: Literal["rf"]
+    object: str
+    time: int
+    added_phase: AddedPhase
+
 
 class AddedPhase(BaseModel):
     type: str
     float: int
 
-class AmplitudeItem(BaseModel):
-    type: str
-    equation: str
 
-class Line(BaseModel):
-    type: str
-    counter: int
+class Adc(Step):
+    action: Literal["adc"]
+    object: str
+    time: int
+    frequency: int
+    phase: int
+    added_phase: AddedPhase  
+    mdh: dict[str, MdhOption]
 
-class FirstScanSlice(BaseModel):
-    type: str
-    counter: int
-    target: int
 
-class LastScanSlice(BaseModel):
+class MdhOption(BaseModel):
     type: str
     counter: int
-    target: int
+    target: Optional[int] = None
 
-class CenterLine(BaseModel):
-    type: str
-    value: int
+class Mark(Step):
+    action: Literal["mark"]
+    time: int
 
-class CenterColumn(BaseModel):
-    type: str
-    value: int
 
-class Mdh(BaseModel):
-    line: Line
-    first_scan_slice: FirstScanSlice
-    last_scan_slice: LastScanSlice
-    center_line: CenterLine
-    center_column: CenterColumn
+class Submit(Step):
+    action: Literal["submit"]
 
-class BlockTR(BaseModel):
-    print_counter: str
-    print_message: str
-    steps: List[Step]
 
-class Instruction(BaseModel):
-    print_counter: Optional[str] = None
-    print_message: str
-    steps: dict[str, Step]
+### objects section
+object_subclass_registry = {}
 
-class RealTimeEvent(BaseModel):
+
+class Object(BaseModel):
     type: str
     duration: int
+    
+    def __init_subclass__(cls, **kwargs: Any) -> None:
+        super().__init_subclass__(**kwargs)
+        object_subclass_registry[cls.__name__] = cls    
 
-class RfExcitation(BaseModel):
+    class Config:
+        extra = "allow" 
+
+
+class HasObjects():
+    def __init__(self, **kwargs):
+        listed_objects = list(kwargs['objects'])
+        for index in range(len(listed_objects)):
+            current_object = listed_objects[index]
+            if isinstance(current_object, dict):
+                item_object_keys = sorted(current_object.keys())
+                for name, cls in object_subclass_registry.items():
+                    registery_step_keys = sorted(cls.model_fields.keys())
+                    if item_object_keys == registery_step_keys:
+                        try:
+                            current_object = cls(**current_object)
+                        except: 
+                            pass
+                        break
+                else:
+                    raise Exception(f"Unknown step action \"{current_object['objects']}\"")
+                listed_objects[index] = current_object
+        super().__init__(**kwargs)
+
+
+class RfExcitation(Object):
     type: Literal["rf"]
     array: str
     duration: int
@@ -119,52 +209,68 @@ class RfExcitation(BaseModel):
     flipangle: int
     purpose: str
 
-class Gradient(RealTimeEvent):
+
+class Gradient(Object):
     type: Literal["grad"]
     array: str
     duration: int
     tail: int
     amplitude: float
 
-class AdcReadout(RealTimeEvent):
+
+class AdcReadout(Object):
     type: Literal["adc"]
     samples: int
     dwelltime: int
 
-class Ttl(RealTimeEvent):
+
+class Ttl(Object):
     type: Literal["sync"]
     duration: int
     event: str
 
+
+### arrays section
 class GradientTemplate(BaseModel):
     encoding: str
     type: str
     size: int
     data: List[float]
 
+
+### equations section
 class Equation(BaseModel):
     equation: str
 
-class PulseSequence(BaseModel):
+
+### definition of entire SDL model
+class PulseSequence(HasObjects, BaseModel):
     file: File
     settings: Settings
     infos: Infos
     instructions: dict[str, Instruction]
-    objects: dict[str, RealTimeEvent]
+    objects: dict[str, SerializeAsAny[Object]]
     arrays: dict[str, GradientTemplate]
     equations: dict[str, Equation]
 
-# Load sequence data
+
+### loading of sequence data
 with open('C:\\Users\\artiga02\\mtrk_seq\\examples\\miniflash.mtrk') as sdlFile:
     sdlData = json.load(sdlFile)
     sequence_data = PulseSequence(**sdlData)
-    print(sequence_data.file.format)
 
-    # Modify a value
-    sequence_data.file.format = "Custom!"
-    print(sequence_data.file.format)
+    # printing debug info
+    # debug(sequence_data.instructions)
 
-# Write json schema to SDL file
-with open('C:\\Users\\artiga02\\mtrk_seq\\examples\\miniflash.mtrk', 'w') as sdlFileOut:
-    sdlFileOut.write(sequence_data.model_dump_json(indent=4))
+    # modifying of a value
+    # sequence_data.file.format = "Custom!"
+    # print(sequence_data.file.format)
 
+
+### writing of json schema to SDL file with formatting options
+with open('C:\\Users\\artiga02\\mtrk_seq\\examples\\test.mtrk', 'w') as sdlFileOut:
+    options = jsbeautifier.default_options()
+    options.indent_size = 4
+    data_to_print = jsbeautifier.beautify(json.dumps(sequence_data.model_dump(mode="json")), options)
+    sdlFileOut.write(re.sub(r'}, {', '},\n            {', data_to_print)) #purely aesthetic 
+  
